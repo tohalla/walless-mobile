a20c1ab2a3e44596bcd6d3376b7fe04015a0c052
Touko Hallasmaa <touko.hallasmaa@outlook.com>
2017-04-20 12:19:28 +0300

    add side menu

--- src/MainNavigator.component.js
+++ src/MainNavigator.component.js
@@ -1,23 +1,73 @@
 // @flow
 import React from 'react';
 import {
-	Navigator
+	Navigator,
+	StyleSheet,
+	View,
+	ScrollView,
+	TouchableOpacity,
+	Text
 } from 'react-native';
+import SideMenu from 'react-native-side-menu';
 
 import Authentication from './account/Authentication.component';
+import colors from './styles/colors';
 
 export default class MainNavigator extends React.Component {
-	renderScene = (route, navigator) =>
-		route.id === 'authentication' ? <Authentication navigator={navigator}/>
-		: null
+	renderScene = (route, navigator) => (
+		<View style={styles.background}>
+			{
+				route.id === 'authentication' ? <Authentication navigator={navigator}/>
+				: <SideMenu
+						menu={(
+							<ScrollView
+									style={{
+								    flex: 1,
+								    width: window.width,
+								    height: window.height,
+								    padding: 10,
+										backgroundColor: colors.sideMenu.background
+									}}
+							>
+								<TouchableOpacity onPress={() => {}}>
+									<Text style={styles.menuButton}>{'menubutton'}</Text>
+								</TouchableOpacity>
+								<TouchableOpacity onPress={() => {}}>
+									<Text style={styles.menuButton}>{'menubutton'}</Text>
+								</TouchableOpacity>
+							</ScrollView>
+						)}
+				>
+					<View style={styles.background}>
+						{
+						}
+					</View>
+				</SideMenu>
+			}
+		</View>
+	);
 	render() {
 		return (
 			<Navigator
 					initialRoute={{id: 'authentication'}}
 					renderScene={this.renderScene}
-					configureScreen={(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom}
+					configureScreen={(route, routeStack) => Navigator.SceneConfigs.PushFromRight}
 			/>
 		);
 	}
 }
 
+const styles = StyleSheet.create({
+	background: {
+    flex: 1,
+		backgroundColor: colors.background,
+    width: window.width,
+    height: window.height
+	},
+	menuButton: {
+		fontSize: 18,
+		padding: 4,
+		color: colors.sideMenu.foreground,
+		width: '100%'
+	}
+});
---
+++ src/styles/colors.js
@@ -0,0 +1,23 @@
+const colors = {
+	alizarin: '#e74c3c',
+	ebonyClay: '#22313F',
+	madison: '#2C3E50',
+	bluewood: '#34495E',
+	white: '#FFFFFF',
+	carrara: '#F2F1EF',
+	gallery: '#EEEEEE',
+	iron: '#DADFE1',
+	lightGray: '#9E9E9E',
+	gray: '#424242',
+	darkGray: '#212121',
+	black: '#000'
+}
+
+export default Object.assign({
+	sideMenu: {
+		background: colors.alizarin,
+		foreground: colors.carrara
+	},
+	background: colors.white,
+	foreground: colors.darkGray,
+}, colors);
