a74cea23a8bead837be40b8258289566c73b4bee
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2017-07-31 10:52:54 +0300

    add logo to navigation drawer

--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -1,5 +1,8 @@
 // @flow
-import {DrawerNavigator} from 'react-navigation';
+import React from 'react';
+import {Image, ScrollView, Platform, View} from 'react-native';
+import {DrawerNavigator, DrawerItems} from 'react-navigation';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
 import {initialRouteName as restaurantRoute} from 'walless/navigation/RestaurantNavigation';
 import {initialRouteName as settingsRoute} from 'walless/navigation/SettingsNavigation';
@@ -7,6 +10,7 @@ import {initialRouteName as ordersRoute} from 'walless/navigation/OrdersNavigati
 import RestaurantNavigation from 'walless/navigation/RestaurantNavigation.component';
 import SettingsNavigation from 'walless/navigation/SettingsNavigation.component';
 import OrdersNavigation from 'walless/navigation/OrdersNavigation.component';
+import {major} from 'walless/styles/spacing';
 
 export const initialRouteName = 'home';
 
@@ -29,8 +33,26 @@ export const routes = {
   }
 };
 
+class ContentComponent extends React.Component {
+  render() {
+    return (
+      <ScrollView contentContainerStyle={styles.navigationContainer}>
+        <View style={styles.logoContainer}>
+          <Image
+              resizeMode="contain"
+              source={require('walless-native/img/walless.png')}
+              style={styles.logo}
+          />
+        </View>
+        <DrawerItems {...this.props} style={{marginTop: 0}} />
+      </ScrollView>
+    );
+  }
+}
+
 const MainNavigation = new DrawerNavigator(routes, {
   initialRouteName,
+  contentComponent: props => <ContentComponent {...props}/>,
   navigationOptions: ({navigation: {state}, screenProps: {titles}}) => ({
     title: titles[state.routeName]
   })
@@ -38,3 +60,21 @@ const MainNavigation = new DrawerNavigator(routes, {
 
 export default MainNavigation;
 
+const styles = EStyleSheet.create({
+  navigationContainer: {
+    marginTop: Platform.OS === 'ios' ? 20 : 0,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    flex: 1
+  },
+  logoContainer: {
+    flex: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 90,
+    marginVertical: major
+  },
+  logo: {
+    height: '100%'
+  }
+});
