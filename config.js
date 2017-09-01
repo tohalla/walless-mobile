d6382a9adee162d003c885d831fd95a6a5849518
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2017-09-01 15:51:46 +0300

    upgrade dependencies

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
+const url = process.env.NODE_ENV === 'production' ? '172.20.10.2' : '172.20.10.2';
+const port = process.env.NODE_ENV === 'production' ? 8080 : 8080;
+
 module.exports = {
-	i18n: {
-		url: 'http://172.20.10.2:8080/translation/'
-	},
+  i18n: {
+    url: 'http://172.20.10.2:8080/translation/'
+  },
   api: {
-    url: process.env.NODE_ENV === 'production' ? '172.20.10.2' : '172.20.10.2',
-    port: process.env.NODE_ENV === 'production' ? 8080 : 8080,
+    url,
+    port,
     protocol: process.env.NODE_ENV === 'production' ? 'https' : 'http',
     graphQL: {
       endpoint: 'graphql'
@@ -15,5 +18,11 @@ module.exports = {
     upload: {
       endpoint: 'upload'
     }
+  },
+  websocket: {
+    url,
+    port,
+    protocol: process.env.NODE_ENV === 'production' ? 'wss' : 'ws',
+    endpoint: undefined
   }
 };
